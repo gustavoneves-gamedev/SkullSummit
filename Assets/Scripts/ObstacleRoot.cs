c1de750f025915cbbf60d39bcb7afa3f8417d66f
Gustavo Neves <gustavo.neves.1995@gmail.com>
2026-03-27 19:31:16 -0300

    update: ObstacleRoot
    Atualizei o script para tentar dar uma rotação aos obstáculos

--- Assets/Scripts/ObstacleRoot.cs
+++ Assets/Scripts/ObstacleRoot.cs
@@ -7,20 +7,37 @@ public class ObstacleRoot : MonoBehaviour
     [SerializeField] private float movementSpeed = 10f;
     [SerializeField] private int obsctacleType = 0;
 
+    private float rotateSpeedA;
+    private float rotateSpeedB;
     private PlayerRoot player;
     //private Collider collider;
-    
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         Destroy(gameObject, 30f);
         player = GameController.gameController.playerRoot;
+        rotateSpeedA = Random.Range(20f, 40f);
+        rotateSpeedB = Random.Range(15f, 25f);
+
+        if (obsctacleType == 1)
+        {
+            transform.position += Vector3.up * 3f;
+
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(obsctacleType == 1) transform.position += Vector3.back * movementSpeed * Time.deltaTime;
+        if (obsctacleType == 1)
+        {
+            transform.position += Vector3.back * movementSpeed * Time.deltaTime;
+
+            transform.Rotate(Vector3.right, rotateSpeedA * Time.deltaTime);
+            transform.Rotate(Vector3.up, rotateSpeedB * Time.deltaTime);
+
+        }
 
         if (GameController.gameController.isRunning) return;
         Destroy(gameObject, 1f);
@@ -36,11 +53,11 @@ public class ObstacleRoot : MonoBehaviour
 
     public void WasShot(GameObject bullet)
     {
-        
+
         Destroy(bullet);
         Destroy(gameObject, 10f);
-        
+
     }
 
-    
+
 }
