3d49f092047e2c495e3375d8acac16a2eb64e78e
Gustavo Neves <gustavo.neves.1995@gmail.com>
2026-03-28 18:07:50 -0300

    fix & optimize: CoinStack
    Corrigi erro do dento de destruição das moedas e otimizei o project settings par ao mobile

--- Assets/Scripts/CoinStack.cs
+++ Assets/Scripts/CoinStack.cs
@@ -5,7 +5,7 @@ public class CoinStack : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Destroy(gameObject, 20f);
+        Destroy(gameObject, 30f);
     }
 
     // Update is called once per frame
