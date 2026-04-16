7b26818aa57edee0852a2655c79e5f66e6d5c885
Gustavo Neves <gustavo.neves.1995@gmail.com>
2026-04-16 15:59:13 -0300

    Update: Items prefab
    Adicionei o script de itens para drops

--- Assets/Scripts/Items.cs
+++ Assets/Scripts/Items.cs
@@ -2,15 +2,15 @@ using UnityEngine;
 
 public class Items : MonoBehaviour
 {
-    [SerializeField] private int itemCode = 0;
+    //[SerializeField] private int itemCode = 0;
 
     [SerializeField] private ParticleSystem VFX;
     [SerializeField] private AudioSource SFX;
-    
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public void PlayFX()
     {
-        VFX.Play();
-        SFX.Play();
+        if (VFX != null) VFX.Play();
+        if (SFX != null) SFX.Play();
     }
 }
