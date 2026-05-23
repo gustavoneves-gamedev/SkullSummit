4ae06e3dff12be7ce701e323f26926d591ec8dbf
Gustavo Neves <gustavo.neves.1995@gmail.com>
2026-05-23 15:56:30 -0300

    Update: Camera Follow
    Atualizei o script da câmera para afastar ao usar o especial e depois voltar ao normal corretamente

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -4,16 +4,28 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     [SerializeField] private Vector3 positionOffset;
-    private Transform targetToFollow;
-    [SerializeField] private float cameraSpeed = 10f;
+    [SerializeField] private float cameraSpeed = 7.5f;
+    [SerializeField] private float specialCameraSpeed = 3f;
     private Quaternion defaultRotation;
     private bool hasCameraChanged;
+
+    [Header("Camera Position")]
+    [SerializeField] private float y = 19.01f;
+    [SerializeField] private float z = 3.54f;
+    private float x;
+
     private PlayerRoot player;
+    private Transform targetToFollow;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        //Debug.Log("Quaternion: " + transform.rotation);
+
+        Invoke("InitializeCamera", .2f);
+    }
+
+    private void InitializeCamera()
+    {
         player = GameController.gameController.playerRoot;
         targetToFollow = player.transform;
         defaultRotation = transform.rotation;
@@ -22,18 +34,41 @@ public class CameraFollow : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Time.timeScale == 0) return;
-        
-        //if(transform == null) return;
+        if (Time.timeScale == 0 || player == null) return;
 
-        float x = Mathf.Lerp(transform.position.x,targetToFollow.position.x + positionOffset.x, cameraSpeed * Time.deltaTime);
+        //currentPosition = defaultPosition;
+        if (GameController.gameController.isRunning) SpecialCameraTransition();
+
+        x = Mathf.Lerp(transform.position.x,targetToFollow.position.x + positionOffset.x,cameraSpeed * Time.deltaTime);
         
-        transform.position = new Vector3(x, transform.position.y, targetToFollow.position.z + positionOffset.z);
+
+        //transform.position = new Vector3(x, transform.position.y, targetToFollow.position.z + positionOffset.z);
+        
+        transform.position = new Vector3(x, targetToFollow.position.y + y, 
+            targetToFollow.position.z + z);
     }
 
     private void SpecialCameraTransition()
     {
+        if (player.playerPowers.isSpecialOn)
+        {
+            //positionOffset.y += 4f;
+            y = Mathf.Lerp(y, 22f, specialCameraSpeed * Time.deltaTime);
 
+            //positionOffset.z -= 3f;
+            z = Mathf.Lerp(z, .5f, specialCameraSpeed * Time.deltaTime);
+            //currentPosition = Vector3.MoveTowards(transform.position, specialDestination, cameraSpeed);
+        }
+        else
+        {
+            //positionOffset.y -= 4f;
+            y = Mathf.Lerp(y, 19.01f, specialCameraSpeed * Time.deltaTime);
+            //positionOffset.z += 3f;
+            z = Mathf.Lerp(z, 3.54f, specialCameraSpeed * Time.deltaTime);
+            //currentPosition = Vector3.MoveTowards(transform.position, defaultPosition, cameraSpeed);
+        }
+
+        //transform.position = currentPosition;
     }
 
     public void ChangeCamera()
